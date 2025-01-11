32a1eff83cecd67def9e243b58f4fa86c2e89337
David Fleming <tonage72@gmail.com>
2025-01-11 01:48:36 -0500

    Just an update

--- game.py
+++ game.py
@@ -1,47 +1,72 @@
 import random
-from actors import Player, Enemy
+from actors import Imp, Player, Enemy, Ogre, Imp
 
-def main():
-    print_intro()
-    play()
+class Game:
+    def __init__(self, player, enemies) -> None:
+        self.player = player
+        self.enemies = enemies
 
-def print_intro():
-    print('''
-        Monster Slash!!!
-        Ready Player One?
-        [Press Enter to Continue]
-    ''')
-    input()
+    def main(self):
+        self.print_intro()
+        self.play()
 
-def play():
-    enemies = [
-        Enemy('Ogre', 1),
-        Enemy('Imp', 1)
-    ]
-    player = Player('Hercules', 1)
-    while True:
-        next_enemy = random.choice(enemies)
-        cmd = input('You see a {}. [r]un, [a]ttack, [p]ass'.format(next_enemy.kind))
-        if cmd == 'r':
-            print('{} runs away!'.format(player.name))
-        elif cmd == 'a':
-            print('{} attack the {}'.format(player.name, next_enemy.kind))
-            if player.attacks(next_enemy):
-                enemies.remove(next_enemy)
+    def print_intro(self):
+        print('''
+            Monster Slash!!!
+            Ready Player One?
+            [Press Enter to Continue]
+        ''')
+        input()
+
+    def print_linebreak(self):
+        print()
+        print('*'*30)
+        print()
+
+    def play(self):
+        while True:
+            next_enemy = random.choice(self.enemies)
+            cmd = input('You see a {}. [r]un, [a]ttack, [p]ass'.format(next_enemy.kind))
+            if cmd == 'r':
+                print('{} runs away!'.format(self.player.name))
+                print('{} heals thyself!'.format(self.player.name))
+                self.player.heal()
+                print(self.player.hp)
+
+            elif cmd == 'a':
+                self.player.attacks(next_enemy)
+                if not next_enemy.is_alive():
+                    self.enemies.remove(next_enemy)
+                    next_enemy = None
+                if next_enemy:
+                    next_enemy.attacks(self.player)
+
+            elif cmd == 'p':
+                print('You are still thinking about your next move...')
+                if random.randint(1, 11) < 5:
+                    next_enemy.attacks(self.player)
+
             else:
-                print('{} hides to plan the next move...'.format(player.name))
-        elif cmd == 'p':
-            print('You are still thinking about your next move...')
-        else:
-            print('Please choose a valid option')
+                print('Please choose a valid option')
 
-        print()
-        print("*"*30)
-        print()
+            if not self.player.is_alive():
+                print('Oh no! You lose!')
+                break
 
-        if not enemies:
-            print('You have won! Congratulations!')
-            break
+            self.print_linebreak()
+            self.player.stats()
+            for e in self.enemies:
+                e.stats()
+            self.print_linebreak()
+
+            if not self.enemies:
+                print('You have won! Congratulations!')
+                break
 
 if __name__ == '__main__':
-    main()
+    enemies = [
+        Ogre('Bob', 1, 3),
+        Imp('Alice', 1)
+    ]
+    player = Player('Hercules', 1)
+    Game(player, enemies).main()
